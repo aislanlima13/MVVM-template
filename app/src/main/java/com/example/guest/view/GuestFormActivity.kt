717f01a8885d485eafe03ad053d4b0ff4576ae91
aislanlima13 <aislan.lima04@gmail.com>
2022-01-10 20:34:54 -0300

    Inicia estrutura do repositório

--- app/src/main/java/com/example/guest/view/GuestFormActivity.kt
+++ app/src/main/java/com/example/guest/view/GuestFormActivity.kt
@@ -4,6 +4,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
+import android.widget.RadioButton
+import android.widget.Toast
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.guest.viewmodel.GuestFormViewModel
 import com.example.guest.R
@@ -11,6 +15,8 @@ import com.example.guest.R
 class GuestFormActivity : AppCompatActivity(), View.OnClickListener {
 
     val btnSave by lazy { findViewById<Button>(R.id.btn_save) }
+    val edtName by lazy { findViewById<EditText>(R.id.edt_name) }
+    val rbPresence by lazy { findViewById<RadioButton>(R.id.rb_presence) }
 
     private lateinit var mViewModel: GuestFormViewModel
 
@@ -21,16 +27,33 @@ class GuestFormActivity : AppCompatActivity(), View.OnClickListener {
         mViewModel = ViewModelProvider(this).get(GuestFormViewModel::class.java)
 
         setListeners()
-    }
-
-    private fun setListeners() {
-        btnSave.setOnClickListener(this)
+        observe()
     }
 
     override fun onClick(v: View?) {
         val id = v?.id
         if (id == R.id.btn_save) {
 
+            val name = edtName.text.toString()
+            val presence = rbPresence.isChecked
+
+            mViewModel.save(name, presence)
         }
     }
+
+    private fun setListeners() {
+        btnSave.setOnClickListener(this)
+    }
+
+    private fun observe() {
+        mViewModel.saveGuest.observe(this, {
+            if (it) {
+                Toast.makeText(applicationContext, "Sucesso", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(applicationContext, "Falha", Toast.LENGTH_SHORT).show()
+            }
+        })
+    }
+
+
 }
