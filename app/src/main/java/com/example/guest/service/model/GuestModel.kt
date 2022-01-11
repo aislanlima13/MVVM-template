b0a645205857f0ae96d72bcee80fb40c99ed5ed0
aislanlima13 <aislan.lima04@gmail.com>
2022-01-10 22:13:59 -0300

    Cria funções do crud

--- app/src/main/java/com/example/guest/service/model/GuestModel.kt
+++ app/src/main/java/com/example/guest/service/model/GuestModel.kt
@@ -1,3 +1,3 @@
 package com.example.guest.service.model
 
-data class GuestModel(var name:String, var presence: Boolean)
+data class GuestModel(val id: Int = 0, var name:String, var presence: Boolean)
--- app/src/main/java/com/example/guest/service/repository/GuestRepository.kt
+++ app/src/main/java/com/example/guest/service/repository/GuestRepository.kt
@@ -1,7 +1,10 @@
 package com.example.guest.service.repository
 
+import android.content.ContentValues
 import android.content.Context
+import com.example.guest.service.constants.DataBaseConstants
 import com.example.guest.service.model.GuestModel
+import java.lang.Exception
 
 class GuestRepository private constructor(context: Context) {
 
@@ -15,6 +18,23 @@ class GuestRepository private constructor(context: Context) {
                 repository = GuestRepository(context)
             }
             return repository
+        }
+    }
+
+    fun save(guest: GuestModel): Boolean {
+        return try {
+            val db = mGuestDataBaseHelper.writableDatabase
+
+            val contentValues = ContentValues()
+            contentValues.put(DataBaseConstants.GUEST.COLUMNS.NAME, guest.name)
+            contentValues.put(DataBaseConstants.GUEST.COLUMNS.PRESENCE, guest.presence)
+
+            db.insert(DataBaseConstants.GUEST.TABLE_NAME, null, contentValues)
+
+            true
+        } catch (e: Exception) {
+            println(e)
+            false
         }
     }
 
@@ -33,15 +53,38 @@ class GuestRepository private constructor(context: Context) {
         return list
     }
 
-    fun save(guest: GuestModel) {
+    fun update(guest: GuestModel): Boolean {
+        return try {
+            val db = mGuestDataBaseHelper.writableDatabase
 
+            val contentValues = ContentValues()
+            contentValues.put(DataBaseConstants.GUEST.COLUMNS.NAME, guest.name)
+            contentValues.put(DataBaseConstants.GUEST.COLUMNS.PRESENCE, guest.presence)
+
+            val selection = DataBaseConstants.GUEST.COLUMNS.ID + " = ?"
+            val args = arrayOf(guest.id.toString())
+
+            db.update(DataBaseConstants.GUEST.TABLE_NAME, contentValues, selection, args)
+
+            true
+        } catch (e: Exception) {
+            println(e)
+            false
+        }
     }
 
-    fun update(guest: GuestModel) {
+    fun delete(id: Int): Boolean {
+        return try {
+            val db = mGuestDataBaseHelper.writableDatabase
+            val selection = DataBaseConstants.GUEST.COLUMNS.ID + " = ?"
+            val args = arrayOf(id.toString())
 
-    }
+            db.delete(DataBaseConstants.GUEST.TABLE_NAME, selection, args)
 
-    fun delete(guest: GuestModel) {
-
+            true
+        } catch (e: Exception) {
+            println(e)
+            false
+        }
     }
 }
