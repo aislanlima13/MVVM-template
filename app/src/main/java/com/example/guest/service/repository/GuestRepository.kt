2bbc8f1652fdccf1e363fc1c3abb5a1adc8a5b8f
aislanlima13 <aislan.lima04@gmail.com>
2022-01-17 22:21:12 -0300

    Implementa função para retornar todos os convidados

--- app/src/main/java/com/example/guest/service/repository/GuestRepository.kt
+++ app/src/main/java/com/example/guest/service/repository/GuestRepository.kt
@@ -75,7 +75,40 @@ class GuestRepository private constructor(context: Context) {
 
     fun getAll(): List<GuestModel> {
         val list: MutableList<GuestModel> = ArrayList()
-        return list
+
+        return try {
+            val db = mGuestDataBaseHelper.readableDatabase
+
+            val projection = arrayOf(
+                DataBaseConstants.GUEST.COLUMNS.ID,
+                DataBaseConstants.GUEST.COLUMNS.NAME,
+                DataBaseConstants.GUEST.COLUMNS.PRESENCE)
+
+            val cursor = db.query(DataBaseConstants.GUEST.TABLE_NAME, projection, null,
+                null, null, null, null)
+
+            if (cursor != null && cursor.count > 0) {
+                while (cursor.moveToNext()) {
+                    val id = cursor.getInt(cursor.getColumnIndex(
+                        DataBaseConstants.GUEST.COLUMNS.ID))
+                    val name = cursor.getString(cursor.getColumnIndex(
+                        DataBaseConstants.GUEST.COLUMNS.NAME))
+                    val presence = (cursor.getInt(cursor.getColumnIndex(
+                        DataBaseConstants.GUEST.COLUMNS.PRESENCE)) == 1)
+
+                    val guest = GuestModel(id, name,presence)
+                    list.add(guest)
+                }
+            }
+
+            cursor?.close()
+
+            list
+        } catch (e: Exception) {
+            println(e)
+            list
+        }
+
     }
 
     fun getPresent(): List<GuestModel> {
