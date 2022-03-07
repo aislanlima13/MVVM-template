37ee400811f0cea874f398d802b1e4ff9d1ca6d6
aislanlima13 <aislan.lima04@gmail.com>
2022-03-07 17:48:10 -0300

    Adiciona room ao projeto

--- app/src/main/java/com/example/guest/viewmodel/GuestFormViewModel.kt
+++ app/src/main/java/com/example/guest/viewmodel/GuestFormViewModel.kt
@@ -1,7 +1,6 @@
 package com.example.guest.viewmodel
 
 import android.app.Application
-import android.content.Context
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
@@ -11,7 +10,7 @@ import com.example.guest.service.repository.GuestRepository
 class GuestFormViewModel(application: Application): AndroidViewModel(application) {
 
     private val mContext = application.applicationContext
-    private val mGuestRepository: GuestRepository = GuestRepository.getInstance(mContext)
+    private val mGuestRepository: GuestRepository = GuestRepository(mContext)
 
     private  var mSaveGuest = MutableLiveData<Boolean>()
     val saveGuest: LiveData<Boolean> = mSaveGuest
@@ -20,7 +19,11 @@ class GuestFormViewModel(application: Application): AndroidViewModel(application
     val guest: LiveData<GuestModel> = mGuest
 
     fun save(id: Int, name: String, presence: Boolean) {
-        val guest = GuestModel(id, name, presence)
+        val guest = GuestModel().apply {
+            this.id = id
+            this.name = name
+            this.presence = presence
+        }
 
         if (id == 0) {
             mSaveGuest.value = mGuestRepository.save(guest)
